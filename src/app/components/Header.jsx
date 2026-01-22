1e2d42ead3e34cc6de70b1569e304d44bcdcdfb1
AMY LOUISE TUCK <amyltuck@hotmail.com>
2026-01-22 16:11:37 +0000

    chore: set up basic components

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import HeaderNav from "./HeaderNav";
+
+export default function Header() {}
+return (
+  <header>
+    <Image src="" alt="" />
+    <HeaderNav />
+  </header>
+);
